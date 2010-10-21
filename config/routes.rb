26296f0c0ba91cc619b5a484fef5c440b5556ee0
Gabriela Alejandra Neila <gab.n84@gmail.com>
2010-10-21 00:33:57 -0300

    se definio routes al controller Posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Blog::Application.routes.draw do
+
+  nameespace :admin do
+    resources :posts
+  end
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
