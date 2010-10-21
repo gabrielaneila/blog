6cdcac3939e105eba3978d528d77ce0c1d811d2d
Gabriela Alejandra Neila <gab.n84@gmail.com>
2010-10-20 23:34:41 -0300

    se agrego valor default a published

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '53aae2c2dfa3b838310d3d97d150e9a1694c351e19ca33c0cb469ba712327bb881d0bbf56177a324f0a2c9baecfe6e8c71e5004ba495d1899ea0bdca924de568'
