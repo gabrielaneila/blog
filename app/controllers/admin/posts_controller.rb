6366046a4d2d3659a1101c934f9e6f4c89baff8a
Gabriela Alejandra Neila <gab.n84@gmail.com>
2010-10-23 00:00:31 -0300

    se definio la accion new, create y se creo la vista new

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -3,4 +3,18 @@ class Admin::PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+  
+  def new
+    @post = Post.new
+  end
+  
+  def create
+   @post = Post.new(params[:post])
+   if @post.save
+     redirect_to admin_posts_path
+   else
+     render :action => :new
+   end 
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Blog::Application.routes.draw do
-
-  nameespace :admin do
-    resources :posts
-  end
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -45,11 +40,11 @@ Blog::Application.routes.draw do
   #   end
 
   # Sample resource route within a namespace:
-  #   namespace :admin do
+     namespace :admin do
   #     # Directs /admin/products/* to Admin::ProductsController
   #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+       resources :posts
+     end
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
